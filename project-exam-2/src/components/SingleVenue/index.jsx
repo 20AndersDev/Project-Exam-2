c5789ce2bfbaa65e2c76470b2f42e650c68a0875
Noroff-Anders <AndNes16878@stud.noroff.no>
2024-05-12 00:58:01 +0200

    fixing deletebutton bug

--- project-exam-2/src/components/SingleVenue/index.jsx
+++ project-exam-2/src/components/SingleVenue/index.jsx
@@ -7,29 +7,47 @@ function DisplaySingleVenue() {
   const queryParams = new URLSearchParams(window.location.search);
   const id = queryParams.get("id");
   const url = `${single_venue}/${id}?_owner=true`;
-
+  console.log(url);
   const { data, isLoading, isError } = useApi(url);
 
   useEffect(() => {
     console.log(data);
   }, [data]);
 
+  // Check if data is still loading or if there's an error
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (isError) {
+  if (isError || !data) {
     return <div>Error fetching data</div>;
   }
 
-  return (
-    <div>
-      <h1>Single Venue</h1>
-      <p>Name: {data.name}</p>
-      <p>Description: {data.description} </p>
-      <DeleteVenue id={id} />{" "}
-    </div>
-  );
+  // Check if data.owner exists and matches the name in localStorage
+  const ownerName = data.owner ? data.owner.name : null;
+  const localStorageName = localStorage.getItem("name");
+  const trimmedLocalStorageName = localStorageName
+    ? localStorageName.replace(/"/g, "")
+    : "";
+
+  if (ownerName === trimmedLocalStorageName) {
+    return (
+      <div>
+        <h1>Single Venue</h1>
+        <p>Name: {data.name}</p>
+        <p>Description: {data.description}</p>
+        <DeleteVenue id={id} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Single Venue</h1>
+        <p>Name: {data.name}</p>
+        <p>Description: {data.description}</p>
+      </div>
+    );
+  }
 }
 
 export default DisplaySingleVenue;
